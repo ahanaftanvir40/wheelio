fefb55bea21618ce062fe86f5cdf0d90f06ab0c2
Ahanaf Tanvir <111105281+ahanaftanvir40@users.noreply.github.com>
2025-11-12 18:58:02 +0600

    fixed structure

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,9 +4,9 @@ import "./global.css";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding/index" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="auth" options={{ headerShown: false }} />
+      <Stack.Screen name="auth/index" options={{ headerShown: false }} />
     </Stack>
   );
 }
